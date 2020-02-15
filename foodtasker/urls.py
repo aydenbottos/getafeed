377e53d01b42f7a6d31a06cba7c8be4c93799e9b
Ayden Bottos <aydenbottos12@gmail.com>
2020-02-15 19:23:23 +1100

    fix

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -15,7 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from foodtaskerapp import views
+from django.contrib.auth.views import LoginView, LogoutView
+from django.conf.urls import url
+
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^restaurant/sign-in/$', LoginView.as_view(template_name='restaurant/sign_in.html'), name = "restaurant-sign-in"),
+    url(r'^restaurant/sign-out', LogoutView.as_view(next_page="/"), name = 'restaurant-sign-out'),
+    url(r'^restaurant/sign-up', views.restaurant_sign_up, name = 'restaurant-sign-out'),
+    url(r'^restaurant/$', views.restaurant_home, name = 'restaurant-home')
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
